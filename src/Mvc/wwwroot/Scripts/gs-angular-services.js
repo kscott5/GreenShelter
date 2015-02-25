b631caedc6643460109ce7a1cb00bf657dc1a1ee
Karega K. Scott <kscott@pcsc.co>
2015-02-25 16:26:35 -0600

    Udated the Client resource

--- src/Mvc/wwwroot/Scripts/gs-angular-services.js
+++ src/Mvc/wwwroot/Scripts/gs-angular-services.js
@@ -4,46 +4,69 @@
 
 $gs.services.add('Client', ['$resource', '$http',
 	function($resource, $http) {
-		var action = {
-			'login': $resource('/api/v1/client/login', {}, {
-				post: {	method: 'POST'}
-			}),
-			'register': $resource('/api/v1/client/register', {}, {
-				post: {	method: 'POST'}
-			}),
-			'get': $resource('/api/v1/client/me', {}, {
-				me: { method: 'GET'},
-				post: {	method: 'POST'}
-			}),
-			'update': $resource('/api/v1/client/me', {}, {
-				me: { method: 'POST'}
-			}),				
-			'authtypes': $resource('/api/v1/client/authtypes', {}, {
-				getProviders: {	method: 'GET'}
-			}),
+		var actions = {
+			'login': function(data,successHandler,errorHandler) {
+				// Use arguments. arguments.length == 1 or arguments.length == 2 
+				// Then you can remove the function parameters
+				$resource('/api/v1/client/login', {}, {
+					post: {	method: 'POST'}
+				}).post(data).$promise.then(successHandler,errorHandler);
+			},
+			'register': function(data,successHandler,errorHandler) {
+				// Use arguments. arguments.length == 1 or arguments.length == 2.
+				// Then you can remove the function parameters
+				$resource('/api/v1/client/register', {}, {
+					post: {	method: 'POST'}
+				}).post(data).$promise.then(successHandler,errorHandler);
+			},
+			'getMe': function(data,successHandler,errorHandler){
+				// Use arguments. arguments.length == 1 or arguments.length == 2 
+				// Then you can remove the function parameters
+				
+				$resource('/api/v1/client/me', {}, {
+					get: { method: "GET" }
+				}).get(data).$promise.then(successHandler,errorHandler);
+			},
+			'updateMe': function(data,successHandler,errorHandler){
+				// Use arguments. arguments.length == 1 or arguments.length == 2 
+				// Then you can remove the function parameters
+				
+				$resource('/api/v1/client/me', {}, {
+					update: { method: "POST" }
+				}).update(data).$promise.then(successHandler,errorHandler);
+			},				
+			'getAuthtypes': function(successHandler,errorHandler) {
+				// Use arguments. arguments.length == 1 or arguments.length == 2 
+				// Then you can remove the function parameters
+				
+				$resource('/api/v1/client/authtypes', {}, {
+					get: { method: 'GET' } 
+				}).get().$promise.then(successHandler,errorHandler);
+			},
 			'external': {
-				'login': $resource('/api/v1/client/externallogin', {}, { 
-					requested: { 
-						method: 'GET',
-						headers: {
-							'Content-Type': 'application/json; charset=utf-8;'
-						}
-					}
-				})
-			}
-		};
+				'login': function(data,successHandler,errorHandler) {
+					// Use arguments. arguments.length == 1 or arguments.length == 2 
+					// Then you can remove the function parameters
+					$resource('/api/v1/client/externallogin', {}, {
+						requested: { method: 'GET', headers: {'Content-Type': 'application/json; charset=utf-8;' } } 
+					}).requested(data).$promise.then(successHandler,errorHandler);
+				} // end login
+			} // end external
+		}; // end actions
 		
-		return action;
+		return actions;
 	}
 ]); // end Client
 
 $gs.services.add('JSONP', ['$resource', 
 	function($resource){
 		var actions = {
-			'data':	$resource('/scripts/gs-data.json', {}, {
-				get: { method: 'GET' }
-			})
-		};
+			'getAppData': function(successHandler,errorHandler) {
+				$resource('/scripts/gs-data.json', {}, {
+					get: { method: 'GET' }
+				}).get().$promise.then(successHandler,errorHandler);
+			}
+		}; // end actions
 		
 		return actions;
 	}
