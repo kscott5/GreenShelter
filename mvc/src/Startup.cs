273feb9bf56be8457e30d4353bd64fb4a763cf92
Karega K. Scott <kscott@pcsc.co>
2016-01-21 16:51:43 -0600

    Updates to support new Main method entry point.

--- mvc/src/Startup.cs
+++ mvc/src/Startup.cs
@@ -158,5 +158,20 @@ namespace PCSC.GreenShelter
             
             app.ApplicationServices.EnsureDbContextCreatedAndSeeded();
         }
+        
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            var config = WebApplicationConfiguration.GetDefault(args);
+
+            var builder = new WebApplicationBuilder()
+                .UseConfiguration(config)
+                .UseStartup<Startup>();
+                
+            var application =   builder.Build();
+
+            application.Run();
+        }
+
     }
 }
