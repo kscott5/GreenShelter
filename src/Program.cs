8694a856e11af8c6cb1ba9782063be09080c2bda
Karega K. Scott <kscott@pcsc.co>
2016-09-23 14:20:43 -0500

    Removed redundent config for Environmen Variables.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace PCSC.GreenShelter
 {
@@ -12,16 +13,9 @@ namespace PCSC.GreenShelter
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
             var host = new WebHostBuilder()
-                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
